f04e98e1e0f0653b9ab263a9b9d5ac7b59247368
ellaiv <iovumihaela4@gmail.com>
2024-07-19 18:42:02 +0300

    Initialize the project and add some logic

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx";
+
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/util/investment.js
@@ -0,0 +1,9 @@
+export function calculateInvestmentResults({
+  initialInvestment,
+  annualInvestment,
+  expectedReturn,
+  duration,
+}) {
+  const annualData = [];
+  let investmentValue = initialInvestment;
+}
