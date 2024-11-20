d6f1e12d3282624be325886f28f1f529b9906ee1
ellaiv <iovumihaela4@gmail.com>
2024-11-20 14:15:54 +0200

    Project improvement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import logo from "../assets/investment-calculator-logo.jpg"; // corectat importul pentru logo
 
 export default function Header() {
   return (
     <header id="header">
-      <img src={logo} alt="Logo showing a money bag" />
       <h1>Investment Calculator</h1>
     </header>
   );
